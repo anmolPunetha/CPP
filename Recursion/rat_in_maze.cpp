2985124807c8cc0f74bee6e7aec1d5fda88d303a
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2021-05-15 23:10:22 +0530

    Update rat_in_maze.cpp

--- Recursion/rat_in_maze.cpp
+++ Recursion/rat_in_maze.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 //m-row
-bool ratInMaze(char maze[10][10], int soln[][10], int i, int j, int m, int n){
+bool ratInMaze(char maze[1000][1000], int soln[][1000], int i, int j, int m, int n){
     if(i==m and j==n){
         soln[m][n]=1;
         //print
-        for(int a=0;a<m;a++){
-            for(int b=0;b<n;b++){
+        for(int a=0;a<=m;a++){
+            for(int b=0;b<=n;b++){
                 cout<<soln[a][b]<<" ";
             }
             cout<<endl;
@@ -24,7 +24,11 @@ bool ratInMaze(char maze[10][10], int soln[][10], int i, int j, int m, int n){
     //assume solun exists through current cell
     soln[i][j]=1;
     bool rightSuccess = ratInMaze(maze, soln, i, j+1, m ,n);
-    bool downSuccess = ratInMaze(maze, soln, i+1, j, m ,n);
+	bool downSuccess;
+	if(!rightSuccess){
+		downSuccess = ratInMaze(maze, soln, i+1, j, m ,n);
+	}
+    
 
     //backtrack
     soln[i][j]=0;
@@ -37,21 +41,17 @@ bool ratInMaze(char maze[10][10], int soln[][10], int i, int j, int m, int n){
 int main() {
     int m,n;
     cin>>m>>n;
-    char maze[10][10];
-    int soln[10][10]={0};
+    char maze[1000][1000];
+    int soln[1000][1000]={0};
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cin>>maze[i][j];
         }
     }
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cout<<maze[i][j];
-        }
-        cout<<endl;
-    }
 
-    bool val = ratInMaze(maze, soln, 0, 0, m, n);
-    cout<<val;
+    bool val = ratInMaze(maze, soln, 0, 0, m-1, n-1);
+    if(val==0){
+		cout<<-1;
+	}
  return 0;
 }
