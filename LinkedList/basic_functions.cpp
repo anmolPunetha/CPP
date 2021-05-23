4e7471129ad94f84c2328d3b156ecb0eb3b19de7
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2021-05-23 12:15:36 +0530

    Update basic_functions.cpp

--- LinkedList/basic_functions.cpp
+++ LinkedList/basic_functions.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+class node{
+    public:
+        int data;
+        node *next;
+        node(int d){
+            data = d;
+            next = NULL;
+        }
+};
+
+void insertAtTail(node* &head, int data){
+    if(head==NULL){
+        head = new node(data);
+        return;
+    }
+
+    node *tail = head;
+    while(tail->next!=NULL){
+        tail = tail->next;
+    }
+    tail->next = new node(data);
+    return ;
+}
+
+void print(node*head){
+    while(head!=NULL){
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+    return;
+}
+int main() {
+    node*head = NULL;
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    print(head);
+    return 0;
+}
