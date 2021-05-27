ce03f7f6e514ada217369f26ab845ba70c0a150d
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2021-05-27 22:53:55 +0530

    Update balanced_paranthesis.cpp

--- Stacks, Queues, Deque/balanced_paranthesis.cpp
+++ Stacks, Queues, Deque/balanced_paranthesis.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include<stack>
+using namespace std;
+int main() {
+    stack <char> s;
+    bool isbalanced = true;
+    char a[10000];
+    cin>>a;
+
+    for(int i=0;a[i]!='\0';i++){
+        char ch = a[i];
+        if(ch=='('){
+            s.push(ch);
+        }
+        else if (ch==')'){
+            if(s.empty() or s.top()!='('){
+                isbalanced=false;
+            }
+            s.pop();
+        }
+    }
+    if(isbalanced){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
