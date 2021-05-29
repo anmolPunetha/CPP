ca3533382a0a638c204d94352c44942e44356c8b
Anmol Punetha <60095765+anmolPunetha@users.noreply.github.com>
2021-05-29 15:42:13 +0530

    Update highest_in_subgroup.cpp

--- Stacks, Queues, Deque/highest_in_subgroup.cpp
+++ Stacks, Queues, Deque/highest_in_subgroup.cpp
@@ -1,31 +1,51 @@
-//pure application of stack
+//pure application of deque
 
 #include <iostream>
-#include<stack>
+#include <deque>
 using namespace std;
+
 int main() {
-    int n,k;
+    int n;
+    int a[100000];
+    int k;
+    
     cin>>n;
-    int a[1000000];
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
     cin>>k;
-    for(int i=0;i<n;i++){
-        if(i+k<=n){
-            stack<int> s;
-            for(int j=i;j<k+i;j++){
-                if(s.empty()){
-                    s.push(a[j]);
-                }
-                else{
-                    if(a[j]>s.top()){
-                        s.push(a[j]);
-                    }
-                }
-            }
-            cout<<s.top()<<" ";
+    
+    //We have to process first k elements separtely
+    deque<int> Q(k);
+    int i;
+    for(i=0;i<k;i++){
+        
+        while(!Q.empty() &&a[i]>a[Q.back()]){
+            Q.pop_back();
         }
+        Q.push_back(i);
+        
     }
+    //Process the remaining elements 
+    for(;i<n;i++){
+        cout<<a[Q.front()]<<" ";
+        
+        //1. Remove the elements which are not the part of the window(Contraction)
+        while((!Q.empty() && (Q.front()<=i-k))){
+            Q.pop_front();
+        }
+        
+        //2. Remove the elements which are not useful and are in window
+        while(!Q.empty() && a[i]>=a[Q.back()]){
+            Q.pop_back();   
+        }
+        
+        //3. Add the new elements(Expansion)
+        Q.push_back(i);
+    }
+    
+    cout<<a[Q.front()]<<endl;
+    
+    
     return 0;
 }
